172655b051c630ff5735ba912ae7a8a043dd3d3f
jamais-vu <46115232+jamais-vu@users.noreply.github.com>
2021-04-02 23:21:35 -0400

    refactor: clean up clickAndHold(). docs: add module explainer

--- js/clickAndHold.js
+++ js/clickAndHold.js
@@ -1,7 +1,9 @@
 /* jshint esversion: 6 */
 
-/* This module contains only one function.
- * I put it in its own module due to its complexity and its generality.
+/* This module contains the function `clickAndHold`.
+ *
+ * It also contains the unused stub `calculateNextDelay` for future expansion.
+ * I put this in its own module due to its complexity and its generality.
  */
 
 /*
@@ -16,46 +18,58 @@
  * (More generally, the "button" could be any arbitrary element, with a handler
  * which calls this in the case of any arbitrary event.)
  */
-export function clickAndHold(button, runOnceFunctions, repeatedFunctions, stopEvent) {
-  // Call each of the "run once" functions immediately, in given order.
+export function clickAndHold(button, runOnceFunctions, repeatedFunctions, stopEvent, initialDelay = 500, minDelay = 50) {
+  /* Call each of the "run once" functions immediately, in given order. */
   for (let func of runOnceFunctions) {
     func();
   }
 
-  // Initialize variables for the time delay between each repeat() call.
-  let holdDelay = 500;  // Initial hold delay (in milliseconds).
-  const minDelay = 50;  // Minimum hold delay (in milliseconds).
-
+  let holdDelay = initialDelay; // Delay (in ms) between repeats
   let timeoutID; // ID of the timer that will be set by setTimeout in repeat().
 
   function repeat() {
-    // Call each of the repeated functions, in given order.
+    /* Call each of the repeated functions, in given order. */
     for (let func of repeatedFunctions) {
       func();
     }
 
-    // Set a delayed call to repeat(), with the current holdDelay.
+    /* Set a delayed call to repeat(), with the current holdDelay. */
     timeoutID = setTimeout(repeat, holdDelay);
 
-    /* Then calculate the next setTimeout delay.
-     * The next delay is halfway between current delay and minimum delay.
-     * This converges asymptotically on the minimum delay, so the user will
-     * feel like it "speeds up" as they hold the button.
-     */
-    // holdDelay = ((holdDelay + minDelay) / 2);
-    // TODO: Actually it feels more snappy to just immediately go fast after
-    // holding down for the initial delay.
-    holdDelay = minDelay;
+    holdDelay = minDelay; // See calculateNextDelay below for potential features
   }
 
-  // When the stopEvent occurs, clears the repeat() function that was queued by
-  // setTimeout.
-  //
-  // We only need to clear the most-recent timeout to stop the loop, because
-  // each setTimeout call is "chained": it only calls the next setTimeout when
-  // the currently-queued one executes, So at any given moment there is only
-  // one setTimeout call queued.
+  /* On stopEvent, clears the repeat() function that was queued by setTimeout.
+   *
+   * We only need to clear the most-recent timeout to stop the loop, because
+   * each setTimeout call is "chained": it only calls the next setTimeout when
+   * the currently-queued one executes, So at any given moment there is only
+   * one setTimeout call queued. */
   button.addEventListener(stopEvent, () => clearTimeout(timeoutID), {once: true});
 
   repeat(); // Call repeat() for the first time, to begin the loop.
+}
+
+/* Helper function for arbitrary delay sequences.
+ *
+ * NOTE: This is not used, but I include it because it's interesting and could
+ * be useful if we use this module in other projects.
+ *
+ * In the clickAndHold function above, we simply set holdDelay to minDelay
+ * immediately, since that feels "snappiest" for the user, but we could set
+ * each subsequent delay using any rule we want, by replacing the line
+ *     `holdDelay = minDelay`
+ * with
+ *     `holdDelay = calculateNextDelay(holdDelay, minDelay)`.
+ *
+ * For example, we could have the difference between the currentDelay and the
+ * minDelay decrease exponentially, for the repeating to start slowly and then
+ * speed up as it approaches a horizontal asymptote at minDelay.
+ *
+ * (Could also replace that line with a generator and call next on it.)
+ */
+function calculateNextDelay(currentDelay, minDelay) {
+  let nextDelay;
+  // Code with Whatever logic you want to use to scale the delays.
+  return nextDelay;
 }
